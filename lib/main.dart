5641e12e2754877addb123a013092aabcad2c26f
vikash423q <vikashkumar@vikashs-MacBook-Air.local>
2020-05-03 01:07:23 +0530

    Login Page created

--- lib/main.dart
+++ lib/main.dart
@@ -1,59 +1,55 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-// import 'Widgets/FormCard.dart';
-// import 'Widgets/SocialIcons.dart';
-// import 'CustomIcons.dart';
+import 'Widgets/FormCard.dart';
+import 'Widgets/SocialIcons.dart';
+import 'CustomIcons.dart';
 
 void main() => runApp(MaterialApp(
-  home: MyApp(),
-  debugShowCheckedModeBanner: false,
-));
+      home: MyApp(),
+      debugShowCheckedModeBanner: false,
+    ));
 
 class MyApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => new _MyAppState();
 }
 
-
 class _MyAppState extends State<MyApp> {
   bool _isSelected = false;
 
-  void toggleRadio() => setState(()=>_isSelected=!_isSelected);
+  void toggleRadio() => setState(() => _isSelected = !_isSelected);
 
   Widget radioButton(bool isSelected) => Container(
-    width: 16.0,
-    height: 16.0,
-    padding: EdgeInsets.all(2.0),
-    decoration: BoxDecoration(
-      shape: BoxShape.circle,
-      border: Border.all(width: 2.0, color: Colors.black)
-    ),
-    child: isSelected
-    ? Container(
-      width: double.infinity,
-      height: double.infinity,
-      decoration: BoxDecoration(
-        shape: BoxShape.circle,
-        color: Colors.black
-      ),
-      )
-      : Container(),
-  );
+        width: 16.0,
+        height: 16.0,
+        padding: EdgeInsets.all(2.0),
+        decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            border: Border.all(width: 2.0, color: Colors.black)),
+        child: isSelected
+            ? Container(
+                width: double.infinity,
+                height: double.infinity,
+                decoration:
+                    BoxDecoration(shape: BoxShape.circle, color: Colors.black),
+              )
+            : Container(),
+      );
 
   Widget horizontalLine() => Padding(
-    padding: EdgeInsets.symmetric(horizontal: 16.0),
-    child: Container(
-      width: ScreenUtil().setWidth(120),
-      height: 1.0,
-      color: Colors.black26.withOpacity(0.2),
-    ),
-  );
+        padding: EdgeInsets.symmetric(horizontal: 16.0),
+        child: Container(
+          width: ScreenUtil().setWidth(120),
+          height: 1.0,
+          color: Colors.black26.withOpacity(0.2),
+        ),
+      );
 
   @override
   Widget build(BuildContext context) {
     ScreenUtil.init(context, width: 750, height: 1334, allowFontScaling: true);
     return new Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: Colors.orange[200].withOpacity(0.8),
       resizeToAvoidBottomPadding: true,
       body: Stack(
         fit: StackFit.expand,
@@ -63,18 +59,117 @@ class _MyAppState extends State<MyApp> {
             children: <Widget>[
               Padding(
                 padding: EdgeInsets.only(top: 28.0),
-                child: Image.asset("assets/image_01.png"),
+                child: Image.asset(
+                  "assets/deliver.png",
+                  width: ScreenUtil().setWidth(360),
+                ),
               ),
-              Expanded(child: Container(),),
+              Expanded(
+                child: Container(),
+              ),
               Padding(
-                padding: EdgeInsets.all(10),
-                child: Image.asset("assets/image_02.png")),
+                  padding: EdgeInsets.all(10),
+                  child: Image.asset("assets/image_02.png")),
             ],
           ),
+          SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.only(left: 28.0, right: 28.0, top: 60.0),
+              child: Column(
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Image.asset(
+                        "assets/logo.png",
+                        width: ScreenUtil().setWidth(110),
+                        height: ScreenUtil().setHeight(110),
+                      ),
+                      Text("Order",
+                          style: TextStyle(
+                              fontFamily: "Poppins-Bold",
+                              fontSize: ScreenUtil().setSp(46),
+                              letterSpacing: 0.6,
+                              fontWeight: FontWeight.bold))
+                    ],
+                  ),
+                  SizedBox(height: ScreenUtil().setHeight(180)),
+                  FormCard(),
+                  SizedBox(height: ScreenUtil().setHeight(40)),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          SizedBox(width: ScreenUtil().setWidth(12.0)),
+                          GestureDetector(
+                              onTap: toggleRadio,
+                              child: radioButton(_isSelected)),
+                          SizedBox(width: ScreenUtil().setWidth(8.0)),
+                          Text("Remember me",
+                              style: TextStyle(
+                                  fontSize: 12, fontFamily: "Poppins-Medium"))
+                        ],
+                      ),
+                      InkWell(
+                        child: Container(
+                          width: ScreenUtil().setWidth(320),
+                          height: ScreenUtil().setHeight(90),
+                          decoration: BoxDecoration(
+                              gradient: LinearGradient(colors: [
+                                Color(0xFF17ead9),
+                                Color(0xFF6078ea)
+                              ]),
+                              borderRadius: BorderRadius.circular(6.0),
+                              boxShadow: [
+                                BoxShadow(
+                                    blurRadius: 8.0,
+                                    offset: Offset(0.0, 8.0),
+                                    color: Color(0xFF6078ea).withOpacity(.3))
+                              ]),
+                          child: Material(
+                            color: Colors.transparent,
+                            child: InkWell(
+                              onTap: () {},
+                              child: Center(
+                                child: Material(
+                                    color: Colors.transparent,
+                                    child: Text("SIGNIN",
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontFamily: "Poppins-Bold",
+                                            fontSize: 18,
+                                            fontWeight: FontWeight.bold,
+                                            letterSpacing: 1.0))),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: ScreenUtil().setHeight(30)),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        "New User? ",
+                        style: TextStyle(fontFamily: "Poppins-Medium"),
+                      ),
+                      InkWell(
+                          onTap: () {},
+                          child: Text("SignUp",
+                              style: TextStyle(
+                                  color: Color(0xFF5d74e3),
+                                  fontFamily: "Poppins-Bold")))
+                    ],
+                  )
+                ],
+              ),
+            ),
+          )
         ],
       ),
     );
-
   }
-
 }
